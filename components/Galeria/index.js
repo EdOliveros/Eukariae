7a91cad4d3f0b584e12336fa3f5e6a7c3cb592d3
ed oliveros <edgarolimoy98@gmail.com>
2023-04-28 21:57:50 -0500

    feat: :art: Bootsrta y Conventional Commits agregados.

--- components/Galeria/index.js
+++ components/Galeria/index.js
@@ -1,36 +1,60 @@
 import React from 'react'
 import imageGaleri from '@public/assets/galeria.png'
 import Image from 'next/image'
+// You can specify which plugins you need
 
 const Galeria = () => {
   return (
-    <div className="galeria-container">      
-      <figure className="imagen">
-        <Image style={{
-          width: '100%',
-          height: 'auto',
-        }} src={imageGaleri} alt="Logo">
-        </Image>
-      </figure>
-
-      <style jsx>{`
-        .galeria-container {
-          width: 100%;
-          height: auto;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          background-color:  #CAC2B5;
-          color: rgb(0, 0, 0);
-          padding: 0 0 20px;
-        }
-        .imagen {
-            margin-bottom: 10px;
-            width: 100%;
-        }
-      `}</style>
+    <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <Image src={imageGaleri} className="d-block w-100" alt="..."></Image>
+        </div>
+        <div className="carousel-item">
+          <Image src={imageGaleri} className="d-block w-100" alt="..."></Image>
+        </div>
+        <div className="carousel-item">
+          <Image src={imageGaleri} className="d-block w-100" alt="..."></Image>
+        </div>
+      </div>
+      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
     </div>
   )
 }
 
 export { Galeria };
+
+
+// <div className="galeria-container">      
+// <figure className="imagen">
+//   <Image style={{
+//     width: '100%',
+//     height: 'auto',
+//   }} src={imageGaleri} alt="Logo">
+//   </Image>
+// </figure>
+
+// <style jsx>{`
+//   .galeria-container {
+//     width: 100%;
+//     height: auto;
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     background-color:  #CAC2B5;
+//     color: rgb(0, 0, 0);
+//     padding: 0 0 20px;
+//   }
+//   .imagen {
+//       margin-bottom: 10px;
+//       width: 100%;
+//   }
+// `}</style>
+// </div>
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import { Layout } from "../components/layout/Layout";
-import '../global-styles/global.css'
+import '../global-styles/global.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+    useEffect(() => {
+        require("bootstrap/dist/js/bootstrap.bundle.min.js");
+      }, []);
+
     return(
         <Layout>
             <Component {...pageProps} />

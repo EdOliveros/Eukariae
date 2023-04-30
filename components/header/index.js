3aa5232c4103c120efb76933080f85437b83d2d3
ed oliveros <edgarolimoy98@gmail.com>
2023-04-29 20:26:20 -0500

    feat: :art: Menu principal actualizado con nav de bootstrap

--- components/header/index.js
+++ components/header/index.js
@@ -1,139 +1,83 @@
 import React from 'react';
 import logo from '../../public/assets/logo.png'
 import Image from 'next/image';
-import menu from '../../public/assets/GRAD0.svg'
 import Link from 'next/link';
-
-function toggleMenu() {
- const menu = document.querySelector('.menu-flotante-container')
- if(menu.classList.contains('inactive')) {
-  menu.classList.remove('inactive')
- } else {
-  menu.classList.add('inactive')
- }
- console.log("escuchando")
-};
 
 
 const Header = () => {
   return (
+    
 
-      <header className="header">
-        <div className="img-container">
+      <>
+
+<nav className="navbar navbar-expand-lg bg-body-tertiary header">
+  <div className="container-fluid">
+  <div className="img-container">
             <figure>
                 <Image style={{
                   width: '60px',
                   height: 'auto',
                 }} src={logo} alt="Logo"></Image>
             </figure>
-        </div>
-        
-        <ul className="menu-container">
-            <li>
-                <Link style={{
+    </div>
+    <button className="navbar-toggler btn" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse justify-content-center" id="navbarNavAltMarkup">
+      <div className="navbar-nav ">
+      <Link
+      className='text-end' style={{
                    fontSize: '17px',
                    margin: '0 20px',
                    textDecoration: 'none',
                    color: 'black',
                    fontWeight: '500',
                 }} href="/">HOME</Link>
-            </li>
-            <li>
-                <Link style={{
+        <Link
+        className='text-end' style={{
                    fontSize: '17px',
-                   margin: '0 6p',
+                   margin: '0 20px',
                    textDecoration: 'none',
                    color: 'black',
                    fontWeight: '500',
                 }}href="/blog">BLOG</Link>
-            </li>
-            <li>
-                <Link style={{
+        <Link
+        className='text-end' style={{
                    fontSize: '17px',
                    margin: '0 20px',
                    textDecoration: 'none',
                    color: 'black',
                    fontWeight: '500',
                 }}href="/productos">PRODUCTS</Link>
-            </li>
-            <li>
-                <Link style={{
+                <Link
+                className='text-end' style={{
                    fontSize: '17px',
                    margin: '0 20px',
                    textDecoration: 'none',
                    color: 'black',
                    fontWeight: '500',
                 }}href="/quienes-somos">QUIENES SOMOS?</Link>
-            </li>
-            <li>
-                <Link style={{
+                <Link
+                className='text-end' style={{
                    fontSize: '17px',
                    margin: '0 20px',
                    textDecoration: 'none',
                    color: 'black',
                    fontWeight: '500',
                 }}href="/contacto">CONTACTO</Link>
-            </li>
-        </ul>
-
-        <div className="div-vacio">
-          <div className='container'>
-            <Image
-            
-            onClick={() => toggleMenu()}
-            
-            style={{
-                  width: '40px',
-                  height: 'auto',
-                }} src={menu} alt="Logo">
-            </Image>
-          </div> 
-        </div>
-        
-
-        <div className="menu-flotante-container inactive">
-        <ul>
-            <li>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}href="/">Home</Link>
-            </li>
-            <li>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}href="/blog">Blog</Link>
-            </li>
-            <li>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}href="/productos">Products</Link>
-            </li>
-            <li>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}href="/quienes-somos">Quienes Somos?</Link>
-            </li>
-            <li>
-                <Link style={{
-                  textDecoration: 'none',
-                  color: 'black',
-                }}href="/contacto">Contacto</Link>
-            </li>
-        </ul>
+      </div>
     </div>
+  </div>
+</nav>
+
+
+        
+        
+       
 
       <style jsx >{`
       
        .header {
-        width: 100%;
-        height: 300px;
-        display: flex;
-        height: 70px;
-        padding: 50px 20px;
         background-color:  #CAC2B5;
         color: rgb(0, 0, 0);
         }
@@ -143,93 +87,19 @@ const Header = () => {
             justify-content: center;
             align-items: center;
         }
-        .menu-container {
-            width: 70vw;
-            display: flex;
-            justify-content: center;
-            align-items: center;
+        .btn {
+          border: none;
         }
-        .menu-container li {
-            list-style: none;
-        }
-        
-        .div-vacio {
-            width: 15vw;
-            display: flex;
-            justify-content: end;
-            align-items: center;
-        }
-        .container {
-          display: flex;
-          justify-content: end;
-          align-items: center;
-        }
-        .container:hover {
-          cursor: pointer;
-        }
-
-        @media (max-width: 900px) {
-          
-          .menu-container li {
-            display: none;
-            }
-          .img-container {
-            width: 30vw;
-            justify-content: start;
-          }
-          .menu-container {
-            width: 40vw;
-          }
-          .div-vacio {
-            width: 30vw;
-        }
-        
-        }
-        @media (min-width: 901px) {
-          .div-vacio .container {
-            display: none;
-            }
-        }
-
-
-
-        .menu-flotante-container {
-          display: flex;
-          flex-direction: column;
-          width: 120px;
-          height: auto;
-          border: 1px solid black;
-          border-radius: 10px;
-          padding: 15px;
-          justify-content: center;
-          align-items: center;
-          background-color: #CAC2B5;
-          position: absolute;
-          top: 107px;
-          right: 10px;
-          z-index: 10;
-        }
-
-        .menu-flotante-container ul {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          flex-direction: column;
-          padding: 0;
-          margin: 0;
-        }
-
-        .inactive {
-          display: none;
-        }
-        .menu-flotante-container ul li {
-          text-align: center;
-          list-style: none;
-          margin-bottom: 5px;
+        .btn:hover,
+        .btn:active,
+        .btn:focus {
+          border: none;
+          outline: none;
+          box-shadow: none;
         }
 
       `}</style>
-     </header>
+     </>
   
   )
 }
